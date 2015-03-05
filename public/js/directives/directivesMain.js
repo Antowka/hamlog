09b4a9ea72d68bbd3cc7eed1ccf2485cd9574944
Anton <662307@gmail.com>
2015-03-06 01:12:08 +0300

    Added directive on datetime for bind changes from Bootstrap datetimepicker to Angular ng-model

--- public/js/directives/directivesMain.js
+++ public/js/directives/directivesMain.js
@@ -1,12 +1,18 @@
 angular.module("formsDirectives", [])
 	//datePicker
 	.directive('datePickerCustom', function () {
-		return function(scope, element, attrs){
-			console.log("test");
+		return function($scope, element){
+
 			$('#datetimepicker').datetimepicker({
 				locale: 'ru',
 				useCurrent: true,
 				format: 'YYYY-MM-DD HH:mm:ss'
 			});
+
+            $(element).blur(function(){
+                $scope.log.date = $(this).val();
+            });
+
+
 		}
 	});
